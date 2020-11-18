2c79e0b2618a929e57df36516c97a3bb271a733f
Marcos <marcos.barreiro.otero@udc.es>
2020-11-18 20:57:58 +0100

    Gráfico para mostrar el porcentage del aforo

--- ClientsApp/app/src/main/java/es/ewic/clients/adapters/ShopRowAdapter.java
+++ ClientsApp/app/src/main/java/es/ewic/clients/adapters/ShopRowAdapter.java
@@ -1,6 +1,8 @@
 package es.ewic.clients.adapters;
 
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -8,7 +10,13 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import androidx.core.graphics.ColorUtils;
 import androidx.fragment.app.Fragment;
+
+import com.ramijemli.percentagechartview.PercentageChartView;
+import com.ramijemli.percentagechartview.callback.AdaptiveColorProvider;
+import com.ramijemli.percentagechartview.callback.OnProgressChangeListener;
+import com.ramijemli.percentagechartview.callback.ProgressTextFormatter;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -66,9 +74,6 @@ public class ShopRowAdapter extends BaseAdapter implements ListAdapter {
 
         TextView shopTitle = convertView.findViewById(R.id.shop_title);
         TextView shopLocation = convertView.findViewById(R.id.shop_location);
-        TextView shop_capacity_status_text = convertView.findViewById(R.id.shop_capacity_status_text);
-
-        ImageView shop_capacity_status = convertView.findViewById(R.id.shop_capacity_status);
 
         JSONObject shop_data = getItem(position);
 
@@ -76,10 +81,38 @@ public class ShopRowAdapter extends BaseAdapter implements ListAdapter {
         if (shop_data != null) {
             shopTitle.setText(type + " - " + shop_data.optString("name"));
             shopLocation.setText(shop_data.optString("location"));
-            shop_capacity_status_text.setText(shop_data.optInt("actualCapacity") + "/" + shop_data.optInt("maxCapacity"));
-
-            shop_capacity_status.setColorFilter(R.color.purple_200);
         }
+
+        PercentageChartView percentageChartView = convertView.findViewById(R.id.shop_percentage);
+
+        float percentage = (float) ((shop_data.optDouble("actualCapacity") / shop_data.optDouble("maxCapacity")) * 100);
+        percentageChartView.setProgress(percentage, false);
+
+        AdaptiveColorProvider colorProvider = new AdaptiveColorProvider() {
+            @Override
+            public int provideProgressColor(float progress) {
+                if (progress < 25) {
+                    return resources.getColor(R.color.semaphore_green);
+                } else if (progress < 100) {
+                    return resources.getColor(R.color.semaphore_ambar);
+                } else {
+                    return resources.getColor(R.color.semaphore_red);
+                }
+            }
+
+            @Override
+            public int provideBackgroundColor(float progress) {
+                //This will provide a bg color that is 80% darker than progress color.
+                return ColorUtils.blendARGB(provideProgressColor(progress), Color.BLACK, .5f);
+            }
+
+            @Override
+            public int provideTextColor(float progress) {
+                return provideProgressColor(progress);
+            }
+        };
+
+        percentageChartView.setAdaptiveColorProvider(colorProvider);
 
         return convertView;
     }
